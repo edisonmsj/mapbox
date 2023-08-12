93829cef4675e5405389e3af4389dcafc1bdd61b
Edison Motta santos Júnior <edison.msj@gmail.com>
2023-08-12 18:21:41 -0300

    correction post input

--- addresses/urls.py
+++ addresses/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import AddressView
+from . import views
 
 urlpatterns = [
-    path('', AddressView.as_view(), name='home')
+    path('', AddressView.as_view(), name='home'),
+    path('search/', views.search_place_view, name='search_place')
+
 ]
